29f396e42f36b57f99d2803349cc2b3707ff5244
bin.shen <bluelazysb@hotmail.com>
2015-06-17 14:22:54 +0800

    checkbox

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -8,6 +8,17 @@ class Test extends MY_Controller {
 	}
 	
 	public function index() {
+		
+		$items = array(
+			array('id' => 1, 'title' => 'Business management', 'type' => 'default', 'author' => '测试1号', 'date' => date('Y-m-d H:i:s')),
+			array('id' => 2, 'title' => 'Business management', 'type' => 'default', 'author' => '测试1号', 'date' => date('Y-m-d H:i:s')),
+			array('id' => 3, 'title' => 'Business management', 'type' => 'default', 'author' => '测试1号', 'date' => date('Y-m-d H:i:s')),
+			array('id' => 4, 'title' => 'Business management', 'type' => 'default', 'author' => '测试1号', 'date' => date('Y-m-d H:i:s')),
+			array('id' => 5, 'title' => 'Business management', 'type' => 'default', 'author' => '测试1号', 'date' => date('Y-m-d H:i:s')),
+			array('id' => 6, 'title' => 'Business management', 'type' => 'default', 'author' => '测试1号', 'date' => date('Y-m-d H:i:s')),
+		);
+		$this->assign('items', $items);
+		
 		$this->show('test');
 	}
 }
