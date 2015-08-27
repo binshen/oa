092ba190c51678de5a222023ec4e1cd1b7b45426
bin.shen <bluelazysb@hotmail.com>
2015-08-27 17:15:11 +0800

    123123

--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -17,9 +17,10 @@ class Main_model extends MY_Model
     
     public function display_bulletin_check() 
     {
+    	$user_info = $this->session->userdata('user_info');
     	$this->db->select('bulletin.id, bulletin.title')->distinct()->from('bulletin_check');
     	$this->db->join('bulletin', 'bulletin_check.bid = bulletin.id', 'inner');
-    	$this->db->where('bulletin_check.uid', $this->session->userdata('user_info')['id']);
+    	$this->db->where('bulletin_check.uid', $user_info['id']);
     	$this->db->where('bulletin_check.status', 1);
     	$this->db->where('bulletin.checked', 0);
     	return $this->db->get()->result_array();
