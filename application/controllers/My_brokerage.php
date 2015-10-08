4bba6b6df51659249a6df80373f4e347fd075912
bin.shen <bluelazysb@hotmail.com>
2015-10-08 16:52:40 +0800

    123123

---
+++ application/controllers/My_brokerage.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class My_brokerage extends MY_Controller {
+	
+	public function __construct() {
+		
+		parent::__construct();
+		
+		$this->load->model('basic_model');
+		$this->load->model('finance_model');
+	}
+	
+	public function list_brokerage($page=1) {
+		
+		$data = $this->finance_model->list_brokerage($page);
+		$base_url = "/my_brokerage/list_brokerage";
+		$pager = $this->pagination->getPageLink($base_url, $data['total'], $data['limit']);
+		$this->assign('pager', $pager);
+		$this->assign('data', $data);
+		
+		$houses = $this->basic_model->get_house_list();
+		$this->assign('houses', $houses);
+		
+		$this->show('/mine/list_brokerage');
+	}
+	
+	public function add_brokerage() {
+		
+		echo "123123123";
+	}
+}
